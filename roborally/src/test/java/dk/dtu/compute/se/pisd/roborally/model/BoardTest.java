aa49809dcacf06676d8e3e4cfa32fd86e140dff4
davidwellejus <s220218@dtu.dk>
2024-05-02 14:06:28 +0200

    Test to see if getNeighbor() works correct

---
+++ roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/model/BoardTest.java
@@ -0,0 +1,36 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.fileaccess.LoadBoard;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class BoardTest {
+    Board board = new Board(8, 8);
+    GameController gameController = new GameController(board);
+
+    @Test
+    void getNeighborTest() {
+        Space startSpace = board.getSpace(4, 4);
+
+        Space northNeighbor = board.getNeighbour(startSpace, Heading.NORTH);
+        assertEquals(board.getSpace(4, 3), northNeighbor);
+
+        Space southNeighbor = board.getNeighbour(startSpace, Heading.SOUTH);
+        assertEquals(board.getSpace(4, 5), southNeighbor);
+
+        Space eastNeighbor = board.getNeighbour(startSpace, Heading.EAST);
+        assertEquals(board.getSpace(5,4), eastNeighbor);
+
+        Space westNeighbor = board.getNeighbour(startSpace, Heading.WEST);
+        assertEquals(board.getSpace(3,4), westNeighbor);
+
+
+        board = LoadBoard.loadBoard("defaultboard");
+        startSpace = board.getSpace(2, 1);
+        southNeighbor = board.getNeighbour(startSpace, Heading.SOUTH);
+        assertNull(southNeighbor);
+
+    }
+}
