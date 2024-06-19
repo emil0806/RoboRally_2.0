48beab0a77b6a301099613c928a88b4ce984bc7b
davidwellejus <s220218@dtu.dk>
2024-06-19 11:23:35 +0200

    moves to startField when moving out of board. Still needs proper testing

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -51,9 +51,13 @@ public class GameController {
         if (player.board == board) {
             Space space = player.getSpace();
             Heading heading = player.getHeading();
-
             Space target = board.getNeighbour(space, heading);
-            if (target != null) {
+
+            if(board.isOutOfMap(space, heading)){
+                moveCurrentPlayerToSpace(player.getStartSpace());
+                player.setHeading(Heading.SOUTH);
+            }
+            else if (target != null) {
                 try {
                     moveToSpace(player, target, heading);
                 } catch (ImpossibleMoveException e) {
@@ -191,7 +195,7 @@ public class GameController {
      * //@author David Wellejus, s220218@dtu.dk
      * //@param space player who should be moved
      */
-    /*public void moveCurrentPlayerToSpace(@NotNull Space space)  {
+    public void moveCurrentPlayerToSpace(@NotNull Space space)  {
         if(space.getPlayer() == null){
             Player currentPlayer = board.getCurrentPlayer();
 
@@ -202,11 +206,11 @@ public class GameController {
             currentPlayer.setSpace(space);
             space.setPlayer(currentPlayer);
 
-            int nextPlayerNumber = (board.getPlayerNumber(currentPlayer) + 1) % board.getPlayersNumber();
-
-            board.setCurrentPlayer(board.getPlayer(nextPlayerNumber));
+//            int nextPlayerNumber = (board.getPlayerNumber(currentPlayer) + 1) % board.getPlayersNumber();
+//
+//            board.setCurrentPlayer(board.getPlayer(nextPlayerNumber));
         }
-    }*/
+    }
 
     private void makeProgramFieldsVisible(int register) {
         if (register >= 0 && register < Player.NO_REGISTERS) {
